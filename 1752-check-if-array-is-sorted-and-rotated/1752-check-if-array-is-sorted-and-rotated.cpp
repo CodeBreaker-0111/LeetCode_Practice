e36585fec0c742af67b554dd3733deaa52decf16
Aaditya Bansal <137265000+CodeBreaker-0111@users.noreply.github.com>
2026-08-02 14:26:58 +0530

    Time: 0 ms (100%), Space: 11.3 MB (18.89%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -5,11 +5,15 @@ public:
         int count = 0;
         for(int i=1;i<n;i++)
         {
+            //Check the descending order
             if(nums[i-1] > nums[i])
             count++;
         }
+        
+            //check the last element and first element of the array
             if(nums[n-1] > nums[0])
             count++;
+
         return count<=1;
     }
 };
