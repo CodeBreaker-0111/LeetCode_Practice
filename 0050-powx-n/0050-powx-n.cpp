f9ff9cbc219b397dd739a56e4919893453379541
Aaditya Bansal <137265000+CodeBreaker-0111@users.noreply.github.com>
2026-08-04 18:34:41 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.86%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    double myPow(double x, int n) 
-    {
-        double ans = 1;
+    double myPow(double x, int n) {
+        double ans=1;
         long nn = n;
-        if(n==0)       //Base Case
-        return 1;
-        
-        if(nn<0)    //If power is negative than do it positive
-        nn = -1 * nn;
 
-        while(nn>0)   //If it is gretaer than 2
+        if(n==0)
+        return 1;
+
+        if(nn<0)
+        nn = -1*nn;
+
+        while(nn>0)
         {
-            //CHECK THE DIVISIBILITY OF 2 because if it is divisible by 2 it will compress the steps by 2
-            if(nn % 2 == 0) 
+            if(nn%2 == 0)
             {
-                x = x*x;     //multiply the x with each other 
-                nn = nn/2;   //power ko kam krdo 2 se
-            }
+                x = x*x;
+                nn = nn/2;
+            }        
             else
             {
-                ans = ans * x;   //Now if not in even than multiply individually and store it in ans
-                nn = nn - 1;     //decrease the power by 1
+                ans = ans*x;
+                nn = nn-1;
             }
         }
-        if(n<0)    //Now to handle the negative power bcz in starting we have converted it in positive
+        
+        if(n<0)
         {
             ans = 1/ans;
         }
